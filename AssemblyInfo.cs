b70f3cde0d57a9502e0fb39b61fc9e59d23ba5cb
leobuitane <90475077+leobuitane@users.noreply.github.com>
2023-04-25 23:54:25 +0200

    Ajoutez des fichiers projet.

---
+++ AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("log discord")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("discord webhook nova-life")]
+[assembly: AssemblyCopyright("NO Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d0561de4-a810-4a00-8b20-c01f55ee003a")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
